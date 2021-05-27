da6393fe6283d63e3f23f0969e417bab28c98889
sTiKyt <stikyt@protonmail.com>
2021-05-27 18:49:22 +0300

    Moved style to it's own place

---
+++ APIe/other/style.py
@@ -0,0 +1,12 @@
+COLORS = {
+    "RED":"\u001b[31m",
+    "GREEN":"\u001b[32m",
+    "CYAN":"\u001b[36m",
+    "RESET":"\u001b[0m",
+    "BOLD":"\u001b[1m"
+}
+bold_green = f"{COLORS['BOLD']}{COLORS['GREEN']}"
+bold_red = f"{COLORS['BOLD']}{COLORS['RED']}"
+bold_cyan = f"{COLORS['BOLD']}{COLORS['CYAN']}"
+reset = COLORS['RESET']
+
