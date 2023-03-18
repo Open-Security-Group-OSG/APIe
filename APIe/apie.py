5d0016fa4fb47e9ac47e35f65ec8f75ec77759e9
sTiKyt <35602029+sTiKyt@users.noreply.github.com>
2023-03-18 20:47:02 +0200

    Replaced print with rich print

--- APIe/apie.py
+++ APIe/apie.py
@@ -1,7 +1,6 @@
 from argparse import ArgumentParser
 import csv
 from textwrap import dedent
-from other.style import bold_cyan, bold_green, bold_red, bold_yellow, reset
 from other.output import open_csv_file, write_to_csv
 from shodan_api.shodan_api import check as check_shodan
 from shodan_api.shodan_api import shodan_basic_keys, shodan_dev_keys, shodan_edu_keys, shodan_oss_keys, shodan_invalid_keys
@@ -11,6 +10,8 @@ from virustotal_api.virustotal_api import check as check_virustotal
 from virustotal_api.virustotal_api import vt_valid_keys, vt_invalid_keys
 from binaryedge_api.binaryedge_api import check as check_binaryedge
 from binaryedge_api.binaryedge_api import binaryedge_free_keys, binaryedge_starter_keys, binaryedge_business_keys, binaryedge_enterprise_keys, binaryedge_invalid_keys
+
+from rich import print
 
 parser = ArgumentParser()
 parser.add_argument("-i", "--input", dest="input_list", metavar="FILE_NAME" , help="Specify shodan API keys list to check, one key per line")
@@ -62,38 +63,38 @@ if __name__ in "__main__":
         write_to_csv('binaryedge', binaryedge_business_keys, output_file)
         write_to_csv('binaryedge', binaryedge_enterprise_keys, output_file)
 
-        print(f'\n{bold_cyan}[+] Valid SHODAN DEV Keys{reset}')
+        print(f'\n[bold cyan][+] Valid SHODAN DEV Keys[/bold cyan]')
         for i in shodan_dev_keys: print(i)
-        print(f'\n{bold_cyan}[+] Valid SHODAN EDU Keys{reset}')
+        print(f'\n[bold cyan][+] Valid SHODAN EDU Keys[/bold cyan]')
         for i in shodan_edu_keys: print(i)
-        print(f'\n{bold_cyan}[+] Valid SHODAN OSS Keys{reset}')
+        print(f'\n[bold cyan][+] Valid SHODAN OSS Keys[/bold cyan]')
         for i in shodan_oss_keys: print(i)
-        print(f'\n{bold_cyan}[+] Valid SHODAN BASIC Keys{reset}')
+        print(f'\n[bold cyan][+] Valid SHODAN BASIC Keys[/bold cyan]')
         for i in shodan_basic_keys: print(i)
-        print(f'\n{bold_cyan}[+] Valid CENSYS Keys{reset}')
+        print(f'\n[bold cyan][+] Valid CENSYS Keys[/bold cyan]')
         for i in censys_valid_keys: print(f'{i[0]} {i[1]}')
-        print(f'\n{bold_cyan}[+] Valid VirusTotal Keys{reset}')
+        print(f'\n[bold cyan][+] Valid VirusTotal Keys[/bold cyan]')
         for i in vt_valid_keys: print(f'{i[0]} {i[1]}')
-        print(f'\n{bold_cyan}[+] Valid BINARYEDGE FREE Keys{reset}')
+        print(f'\n[bold cyan][+] Valid BINARYEDGE FREE Keys[/bold cyan]')
         for i in binaryedge_free_keys: print(i)
-        print(f'\n{bold_cyan}[+] Valid BINARYEDGE STARTER Keys{reset}')
+        print(f'\n[bold cyan][+] Valid BINARYEDGE STARTER Keys[/bold cyan]')
         for i in binaryedge_starter_keys: print(i)
-        print(f'\n{bold_cyan}[+] Valid BINARYEDGE BUSINESS Keys{reset}')
+        print(f'\n[bold cyan][+] Valid BINARYEDGE BUSINESS Keys[/bold cyan]')
         for i in binaryedge_business_keys: print(i)
-        print(f'\n{bold_cyan}[+] Valid BINARYEDGE ENTERPRISE Keys{reset}')
+        print(f'\n[bold cyan][+] Valid BINARYEDGE ENTERPRISE Keys[/bold cyan]')
         for i in binaryedge_enterprise_keys: print(i)
 
-        print(f"\nTotal keys: {bold_cyan}{len(keys_to_check)}{reset}")
-        print(" ".join(dedent(f"""{bold_cyan}[SHODAN]{reset} 
-            DEV keys: {bold_green}{len(shodan_dev_keys)}{reset}, 
-            EDU keys: {bold_green}{len(shodan_edu_keys)}{reset}, 
-            OSS keys: {bold_green}{len(shodan_oss_keys)}{reset}, 
-            BASIC keys: {bold_green}{len(shodan_basic_keys)}{reset}""").replace('\n', '').split()))
-        print(f"{bold_cyan}[CENSYS]{reset} Valid keys: {bold_green}{len(censys_valid_keys)}{reset}")
-        print(f"{bold_cyan}[VIRUSTOTAL]{reset} Valid keys: {bold_green}{len(vt_valid_keys)}{reset}")
-        print(" ".join(dedent(f"""{bold_cyan}[BINARYEDGE]{reset} 
-            FREE keys: {bold_green}{len(binaryedge_free_keys)}{reset}, 
-            STARTER keys: {bold_green}{len(binaryedge_starter_keys)}{reset}, 
-            BUSINESS keys: {bold_green}{len(binaryedge_business_keys)}{reset}, 
-            ENTERPRISE keys: {bold_green}{len(binaryedge_enterprise_keys)}{reset}""").replace('\n', '').split()))
-        print(f"Invalid keys: {bold_red}{len(censys_invalid_keys)}{reset}")
+        print(f"\nTotal keys: [bold cyan]{len(keys_to_check)}[/bold cyan]")
+        print(" ".join(dedent(f"""[bold cyan][SHODAN][/bold cyan]
+            DEV keys: [bold green]{len(shodan_dev_keys)}[/bold green], 
+            EDU keys: [bold green]{len(shodan_edu_keys)}[/bold green], 
+            OSS keys: [bold green]{len(shodan_oss_keys)}[/bold green], 
+            BASIC keys: [bold green]{len(shodan_basic_keys)}[/bold green]""").replace('\n', '').split()))
+        print(f"[bold cyan][CENSYS][/bold cyan] Valid keys: [bold green]{len(censys_valid_keys)}[/bold green]")
+        print(f"[bold cyan][VIRUSTOTAL][/bold cyan] Valid keys: [bold green]{len(vt_valid_keys)}[/bold green]")
+        print(" ".join(dedent(f"""[bold cyan][BINARYEDGE][/bold cyan] 
+            FREE keys: [bold green]{len(binaryedge_free_keys)}[/bold green], 
+            STARTER keys: [bold green]{len(binaryedge_starter_keys)}[/bold green], 
+            BUSINESS keys: [bold green]{len(binaryedge_business_keys)}[/bold green], 
+            ENTERPRISE keys: [bold green]{len(binaryedge_enterprise_keys)}[/bold green]""").replace('\n', '').split()))
+        print(f"Invalid keys: [bold red]{len(censys_invalid_keys)}[/bold red]")
