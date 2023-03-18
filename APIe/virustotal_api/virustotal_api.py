cba41d56bfb93cb1f7ee66ed51da9e867fd17ce4
sTiKyt <35602029+sTiKyt@users.noreply.github.com>
2023-03-18 17:42:53 +0200

    Removed dependency on pypi 'virustotal-python' package

--- APIe/virustotal_api/virustotal_api.py
+++ APIe/virustotal_api/virustotal_api.py
@@ -1,4 +1,4 @@
-from virustotal_python import Virustotal
+from requests import get
 from other.style import bold_green, bold_red, bold_yellow, bold_cyan, reset
 
 vt_valid_keys = []
@@ -7,8 +7,7 @@ vt_invalid_keys = []
 
 def check(key: str):
     try:
-        vt_query = Virustotal(key).request(f'users/{key}')
-        allowance = vt_query.json()['data']['attributes']['quotas']['api_requests_daily']['allowed']
+        allowance = get(f'https://virustotal.com/api/v3/users/{key}', headers={'x-apikey': key}).json()['data']['attributes']['quotas']['api_requests_daily']['allowed']
         print(f'{key} is a {bold_green}VALID{reset} {bold_yellow}VIRUSTOTAL KEY{reset} and daily requests quota is {bold_cyan}{allowance}{reset}')
         return [key, True, allowance]
     except:
