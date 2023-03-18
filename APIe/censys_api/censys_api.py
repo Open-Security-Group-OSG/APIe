56728a1abf55cb09e62aa8b84279639caa9a1e1b
sTiKyt <35602029+sTiKyt@users.noreply.github.com>
2023-03-18 14:20:45 +0200

    Fixed censys_api errors caused by censys library updates

--- APIe/censys_api/censys_api.py
+++ APIe/censys_api/censys_api.py
@@ -1,16 +1,18 @@
-from other.style import bold_green, bold_red, bold_cyan, reset
-from censys.search import CensysIPv4
+from other.style import bold_green, bold_red, bold_cyan, bold_yellow, reset
+from censys.search.v2.api import CensysSearchAPIv2
 import json
 
+censys_valid_keys = []
+censys_invalid_keys = []
 
 
 def check(id_and_secret: str):
     id_and_secret_list = id_and_secret.split(':')
     try:
-        allowance = CensysIPv4(api_id=id_and_secret_list[0], api_secret=id_and_secret_list[1]).quota()
+        allowance = CensysSearchAPIv2(api_id=id_and_secret_list[0], api_secret=id_and_secret_list[1]).quota()
         json_acceptable = str(allowance).replace("'", '"')
         allowance = json.loads(json_acceptable)['allowance']
-        print(f'{bold_cyan}ID:{reset} {id_and_secret_list[0]} {bold_cyan}SECRET:{reset} {id_and_secret_list[1]} is {bold_green}VALID{reset} and has allowance of {bold_cyan}{allowance}{reset}')
+        print(
             f'{bold_cyan}ID:{reset} {id_and_secret_list[0]} {bold_cyan}SECRET:{reset} {id_and_secret_list[1]} is a {bold_green}VALID{reset} {bold_yellow}CENSYS KEY{reset} and has allowance of {bold_cyan}{allowance}{reset}')
         return [id_and_secret, True, allowance]
     except:
